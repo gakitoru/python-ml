67d5d1255749e49c4b6a11cc91ce65204b10e76f
Toru Otagaki <toru@example.com>
2021-04-15 22:17:08 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from util import *
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -8,17 +9,26 @@ X = df.iloc[0:100, [0,2]].values
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - learning rate 0.01')
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
 
-ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.cost_) + 1), np.log10(ada2.cost_), marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('log(Sum-squared-error)')
-ax[1].set_title('Adaline - learning rate 0.0001')
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
 
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+
+plt.tight_layout()
 plt.show()
