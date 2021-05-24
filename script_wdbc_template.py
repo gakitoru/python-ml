6608b73668f19b025929081d5c33ba66af5398ae
Toru Otagaki <toru@example.com>
2021-05-25 08:23:05 +0900

    update

--- script_wdbc_template.py
+++ script_wdbc_template.py
@@ -5,4 +5,15 @@ X = df.loc[:, 2:].values
 y = df.loc[:, 1].values
 le = LabelEncoder()
 y = le.fit_transform(y)
-le.classes_
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=1)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        PCA(n_components=2),
+                        LogisticRegression(random_state=1))
