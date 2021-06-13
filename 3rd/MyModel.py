1211e906c832d5feb99228cd1a7089c7e2e03742
t o <toru@example.com>
2021-06-13 15:37:18 +0900

    update

---
+++ 3rd/MyModel.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.w = tf.Variable(0.0, name='weight')
+        self.b = tf.Variable(0.0, name='bias')
+
+    def call(self, x):
+        return self.w * x + self.b
